7cabea8a06fe797d7647ccaa888eca79e3b878e8
Yaroslav Borys <borrline@gmail.com>
2021-11-11 12:18:14 +0200

    yazloyyyy

--- src/js/fetchCountries.js
+++
@@ -1,9 +0,0 @@
-export default { fetchCountry };
-
-async function fetchCountry(nameCountry) {
-  const response = await fetch(`${BASE_URL}${nameCountry}`);
-  return await response.json();
-}
-
-const BASE_URL = 'https://restcountries.com/v2/name/';
-
